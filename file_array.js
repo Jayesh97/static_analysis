2f5b0e2cea8343861557a31e85c1a14114b9a8e4
sjbondu <sjbondu@ncsu.edu>
2020-04-03 13:13:42 +0000

    t

--- file_array.js
+++ file_array.js
@@ -7,10 +7,11 @@ function traverse_with_dir(dir_path, result) {
   result = result
   files.forEach(function(file) {
     if (fs.statSync(dir_path + "/" + file).isDirectory()) {
+        // console.log(dir_path,__dirname)
         result = traverse_with_dir(dir_path + "/" + file, result)
     } 
     else{
-        if(file.endsWith('.js')){
+        if(file.endsWith('.js') && dir_path.includes("node_modules")===false){
             result.push(path.join(dir_path, "/", file))
         }
     }
@@ -18,4 +19,7 @@ function traverse_with_dir(dir_path, result) {
   return result
 }
 
+res = traverse_with_dir("/home/sjbondu/Complexity/app/checkbox.io/server-side",[]);
+// console.log(res)
+// console.log("i have node_modules".includes("node_odules"))
 exports.traverse_with_dir = traverse_with_dir;

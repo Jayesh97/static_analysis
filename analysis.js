8ce7ec69bbdf828744432bf9eabe2990a4ac1352
sjbondu <sjbondu@ncsu.edu>
2020-04-04 06:47:23 +0000

    tab

--- analysis.js
+++ analysis.js
@@ -29,9 +29,13 @@ function main()
 	// console.log(builders)
 
 	if(errors.length>0){
-		console.log("----------`````````Errors Start Here```````---------------`````````Errors Start Here```````---------------```````````Errors Start Here```````----------");
+		console.log("----------`````````Errors Start Here```````---------------`````````Errors Start Here```````---------------```````````Errors Start Here```````----------\n");
+		console.log("----------`````````Errors Start Here```````---------------`````````Errors Start Here```````---------------```````````Errors Start Here```````----------\n");
+		console.log("----------`````````Errors Start Here```````---------------`````````Errors Start Here```````---------------```````````Errors Start Here```````----------\n\n\n");
 		errors.forEach(element => console.log(chalk.red(element)));
-		console.log("----------`````````Errors End Here```````---------------`````````Errors End Here```````---------------```````````Errors End Here```````----------");
+		console.log("\n\n\n----------`````````Errors End Here```````---------------`````````Errors End Here```````---------------```````````Errors End Here```````----------\n");
+		console.log("\n\n\n----------`````````Errors End Here```````---------------`````````Errors End Here```````---------------```````````Errors End Here```````----------\n");
+		console.log("\n\n\n----------`````````Errors End Here```````---------------`````````Errors End Here```````---------------```````````Errors End Here```````----------\n");
 		throw console.error("The Threshold values are voilated check the above error list");	
 	}
 
